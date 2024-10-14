6993e121f9c7a71437d03a930572064515344250
i7 <bakasa.andrew@gmail.com>
2024-10-14 13:09:10 +0200

    first commit 2

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+export default {
+  providers: [
+    {
+      domain: 
+      //"https://quiet-llama-40.clerk.accounts.dev",
+      "https://definite-wildcat-30.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
